40321a3f42a7ff4b7503c9825a36807c6cffd1f5
gretareddy <greta.parumati@gmail.com>
2019-07-15 14:40:07 -0400

    excelgen

--- myapp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-# Register your models here.
-admin.site.register(Size)
-admin.site.register(Piping)
-admin.site.register(Material)
-admin.site.register(Accessories)
-admin.site.register(Accessories_size)
